0b40a538306bb36ee3a11fe48d2a2ea8c0d359c6
Marcus7428 <marcus.guo@student.tdsb.on.ca>
2025-03-20 00:12:57 +0000

    added heron formula

--- Script.js
+++ Script.js
@@ -1,8 +1,16 @@
-function heronFormula(event) {
-    let a = 0;
-    let b = 0;
-    let c = 0;
-    return Math.sqrt(a * a + b * b);
+function heronFormula(a, b, c) {
+    let s = (a + b + c) / 2;
+    if ((a < b + c) && (b < a + c) && (c < a + b)) {
+        let result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    } else {
+        return `This triangle is not valid`;
+    }
 }
-// console.log(`Test`);
-document.write('Test');
+document.getElementById('heronFormula').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let a = document.getElementById("sideA").value;
+    let b = document.getElementById("sideB").value;
+    let c = document.getElementById("sideC").value;
+    let result = heronFormula(a,b,c);
+    document.getElementById('result').value = result;
+})
