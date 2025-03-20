d99e26e9468145099c51690db8052818f362f0cf
Marcus <marcus.guo@student.tdsb.on.ca>
2025-03-20 01:42:15 -0400

    finished heron formula

--- Script.js
+++ Script.js
@@ -1,16 +1,16 @@
 function heronFormula(a, b, c) {
     let s = (a + b + c) / 2;
     if ((a < b + c) && (b < a + c) && (c < a + b)) {
-        let result = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c)).toString();
     } else {
         return `This triangle is not valid`;
     }
 }
 document.getElementById('heronFormula').addEventListener('submit', function (event) {
     event.preventDefault();
-    let a = document.getElementById("sideA").value;
-    let b = document.getElementById("sideB").value;
-    let c = document.getElementById("sideC").value;
+    let a = parseFloat(document.getElementById("sideA").value);
+    let b = parseFloat(document.getElementById("sideB").value);
+    let c = parseFloat(document.getElementById("sideC").value);
     let result = heronFormula(a,b,c);
     document.getElementById('result').value = result;
 })
