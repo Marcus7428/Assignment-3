c0e9e01c6c1f5e50bf0640ef32c64061824bd4e9
unknown <342636396@TDSB.ON.CA>
2025-03-20 11:15:56 -0400

    finished heron formula for real this time

--- Script.js
+++ Script.js
@@ -1,16 +1,13 @@
-function heronFormula(a, b, c) {
-    let s = (a + b + c) / 2;
-    if ((a < b + c) && (b < a + c) && (c < a + b)) {
-        return Math.sqrt(s * (s - a) * (s - b) * (s - c)).toString();
-    } else {
-        return `This triangle is not valid`;
-    }
-}
-document.getElementById('heronFormula').addEventListener('submit', function (event) {
+document.getElementById('heron').addEventListener('submit', function (event) {
     event.preventDefault();
     let a = parseFloat(document.getElementById("sideA").value);
     let b = parseFloat(document.getElementById("sideB").value);
     let c = parseFloat(document.getElementById("sideC").value);
-    let result = heronFormula(a,b,c);
-    document.getElementById('result').value = result;
+    let s = (a + b + c) / 2;
+    if ((a < b + c) && (b < a + c) && (c < a + b)) {
+        document.getElementById("result").value = Math.sqrt(s * (s - a) * (s - b) * (s - c)).toString();
+    } else {
+        document.getElementById("result").value = "This triangle is not valid";
+    }
+
 })
